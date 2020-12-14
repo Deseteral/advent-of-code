c1c6ce46fdc6758af3d7507a257e3c5de26999bf
Deseteral <deseteral@gmail.com>
2020-12-14 05:58:57 +0100

    Python template

---
+++ templates/python/solve.py
@@ -0,0 +1,11 @@
+#!/usr/local/bin/python3
+import re
+import math
+from collections import defaultdict
+from collections import Counter
+import itertools
+from copy import deepcopy
+
+# with open('test_input') as f:
+with open('input') as f:
+    lines = f.read().splitlines()
