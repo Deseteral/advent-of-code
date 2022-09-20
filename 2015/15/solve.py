bb684ea4439164cefb1ca534dfafce747dca9c91
Deseteral <deseteral@gmail.com>
2022-09-20 22:30:46 +0200

    2015 15

--- 2015/15/solve.py
+++ 2015/15/solve.py
@@ -1,52 +1,25 @@
 #!/usr/local/bin/python3
-import re
-import math
-from collections import defaultdict
 
-def sums(length, total_sum):
-    if length == 1:
-        yield (total_sum,)
-    else:
-        for value in range(total_sum + 1):
-            for permutation in sums(length - 1, total_sum - value):
-                yield (value,) + permutation
+bestscore = 0
+bestscore_500 = 0
 
-def get_products_value(products_amount):
-    global products
+for s in range(100 + 1):
+    for p in range(100 + 1):
+        for f in range(100 + 1):
+            for ss in range(100 + 1):
+                if ((s + p + f + ss) != 100): continue
 
-    totals = [0 for x in range(len(products))]
+                capacity = max(((5 * s) + (-1 * p) + (0 * f) + (-1 * ss)), 0)
+                durability = max(((-1 * s) + (3 * p) + (-1 * f) + (0 * ss)), 0)
+                flavor = max(((0 * s) + (0 * p) + (4 * f) + (0 * ss)), 0)
+                texture = max(((0 * s) + (0 * p) + (0 * f) + (2 * ss)), 0)
 
-    for i in range(len(products)):
-        totals[i] += (products[i][0] * products_amount[i])
-        totals[i] += (products[i][1] * products_amount[i])
-        totals[i] += (products[i][2] * products_amount[i])
-        totals[i] += (products[i][3] * products_amount[i])
+                calories = (s * 5) + (p * 1) + (f * 6) + (ss * 8)
 
-    print(totals)
+                score = capacity * durability * flavor * texture
 
-    value = 1
-    for i in map(lambda x: x if x > 0 else 0, totals):
-        value *= i
+                bestscore = max(bestscore, score)
+                if (calories == 500): bestscore_500 = max(bestscore_500, score)
 
-    return value
-
-
-products = []
-
-with open('input') as f:
-    lines = f.read().splitlines()
-
-    for line in lines:
-        groups = re.match(r"^(\w+)\: capacity (-?\d+), durability (-?\d+), flavor (-?\d+), texture (-?\d+), calories (-?\d+)$", line).groups()
-
-        name = groups[0]
-        features = [int(x) for x in groups[1:-1]]
-
-        products.append(features)
-
-    permutations = list(sums(len(products), 100))
-    best_value = -1
-    for p in permutations:
-        best_value = max(get_products_value(p), best_value)
-
-    print(best_value)
+print(bestscore)
+print(bestscore_500)
