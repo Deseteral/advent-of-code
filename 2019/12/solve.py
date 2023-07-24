eca4eb368b52cd0f51d55e21fe6a20cf662d466b
Deseteral <deseteral@gmail.com>
2023-07-24 22:40:10 +0200

    2019 12

--- 2019/12/solve.py
+++ 2019/12/solve.py
@@ -1,13 +1,7 @@
 #!/usr/local/bin/python3
 import re
 import math
-from collections import defaultdict
-from collections import Counter
 import itertools
-from copy import deepcopy
-import numpy as np
-
-# def add_vectors(a, b): return (a[0] + b[0], a[1] + b[1], a[2] + b[2], a[3] + b[3])
 
 def calc_energy(moon):
     potential = abs(moon[0][0]) + abs(moon[0][1]) + abs(moon[0][2])
@@ -16,7 +10,6 @@ def calc_energy(moon):
 
 def main():
     file = open('input')
-    # file = open('test_input')
     lines = file.read().splitlines()
 
     moons = []
@@ -28,7 +21,12 @@ def main():
             [0, 0, 0],
         ))
 
-    for _time_step in range(1000):
+    total_energy = -1
+
+    axis_hashes = [set(), set(), set()]
+    repetition_idx = [None, None, None]
+
+    for time_step in itertools.count():
         # apply gravity
         for (moon_a, moon_b) in itertools.combinations(moons, 2):
             pos_a = moon_a[0]
@@ -49,12 +47,24 @@ def main():
             for dimension in range(0, 3):
                 moon[0][dimension] += moon[1][dimension]
 
-    for moon in moons:
-        potential = abs(moon[0][0]) + abs(moon[0][1]) + abs(moon[0][2])
-        kinetic = abs(moon[1][0]) + abs(moon[1][1]) + abs(moon[1][2])
-        energy = potential * kinetic
+        if time_step == 1000:
+            total_energy = sum(map(calc_energy, moons))
 
-    total_energy = sum(map(calc_energy, moons))
+        for dimension in range(0, 3):
+            if repetition_idx[dimension] is not None:
+                continue
+
+            axis_hash = str([(moon[0][dimension], moon[1][dimension]) for moon in moons])
+
+            if axis_hash in axis_hashes[dimension]:
+                repetition_idx[dimension] = time_step
+            else:
+                axis_hashes[dimension].add(axis_hash)
+
+        if None not in repetition_idx:
+            break
+
     print(total_energy)
+    print(math.lcm(*repetition_idx))
 
 if __name__ == '__main__': main()
