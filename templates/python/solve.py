09f4f865e8a25f39bc7598444d926f3bdc83fda1
Deseteral <deseteral@gmail.com>
2023-11-30 21:29:44 +0100

    Template update

--- templates/python/solve.py
+++ templates/python/solve.py
@@ -1,15 +1,18 @@
 #!/usr/local/bin/python3
-import re
-import math
-from collections import defaultdict
-from collections import Counter
-import itertools
-from copy import deepcopy
-import numpy as np
+# import re
+# import math
+# from collections import defaultdict
+# from collections import Counter
+# import itertools
+# from copy import deepcopy
+# import numpy as np
+
 
 def main():
     file = open('input')
     # file = open('test_input')
     lines = file.read().splitlines()
 
-if __name__ == '__main__': main()
+
+if __name__ == '__main__':
+    main()
